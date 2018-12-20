2b1d175c0cef8caa6ff072bb62c6431fe926a664
Saurabh Jadhav <40573988+saurabhthesuperhero@users.noreply.github.com>
2018-12-20 15:13:47 +0530

    Done added c code too
    finally working code

--- img.py
+++ img.py
@@ -1,13 +1,22 @@
 from cv2 import imread
 import numpy as np
-img=imread('pic.png')
+f=open("cfile.cpp","w+")
+img=imread('temp.png')
+
+f.write('#include<iostream.h>\n#include<conio.h>\n#include <graphics.h>\nvoid main()\n{ int i;\nint gd=DETECT,gm,color;\n initgraph(&gd,&gm,"c://turboc3//bgi");\n')
 x=[]
 y=np.array([0,0,0])
 for i in range(0,img.shape[0]):
     for j in range(0,img.shape[1]):
 
         if np.array_equal(img[i][j], [0,0,0]) is True: 
-            print('Cordinates:({},{}) rgbcode:{}'.format(i,j,img[i][j]))
+            #print('Cordinates:({},{}) rgbcode:{}'.format(i,j,img[i][j]))
+            yup='\nputpixel({},{},WHITE);'.format(i,j)
+ #           print(yup)
+            f.write(yup)
             
+f.write("getch();\n\nclosegraph(); \n}")
+f.close()
 print("ok")
+
 
