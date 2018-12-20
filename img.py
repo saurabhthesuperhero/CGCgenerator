14da7aa54e066c5b036175a03ca4af79e30c1f7d
Saurabh Jadhav <40573988+saurabhthesuperhero@users.noreply.github.com>
2018-12-20 14:23:26 +0530

    added divied pxels

--- img.py
+++ img.py
@@ -1,8 +1,13 @@
 from cv2 import imread
+import numpy as np
 img=imread('pic.png')
+x=[]
+y=np.array([0,0,0])
 for i in range(0,img.shape[0]):
     for j in range(0,img.shape[1]):
-        print('Cordinates:({},{}) rgbcode:{}'.format(i,j,img[i][j]))
 
-
+        if np.array_equal(img[i][j], [0,0,0]) is True: 
+            print('Cordinates:({},{}) rgbcode:{}'.format(i,j,img[i][j]))
+            
+print("ok")
 
