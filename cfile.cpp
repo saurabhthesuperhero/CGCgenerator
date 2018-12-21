97a648081dd6925d592cb4127591a9722c1d5369
purujeet <spurujeet@gmail.com>
2018-12-22 00:14:52 +0530

    Clean the code and added loop for white color

---
+++ cfile.cpp
@@ -0,0 +1,235 @@
+#include<iostream.h>
+ #include<conio.h>
+ #include <graphics.h>
+ void main()
+ { int i,x,y;
+ int gd=DETECT,gm,color;
+ initgraph(&gd,&gm,"c://turboc3//bgi");
+
+for (x=47;x<135;x++){
+for(int y=270;y<1419;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(135,71,(191,191,191));
+putpixel(135,72,(135,135,135));
+putpixel(135,73,(88,88,88));
+putpixel(135,74,(32,32,32));
+putpixel(135,104,(32,32,32));
+putpixel(135,105,(104,104,104));
+putpixel(135,106,(191,191,191));
+for (x=135;x<136;x++){
+for(int y=209;y<218;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(136,62,(191,191,191));
+putpixel(136,63,(135,135,135));
+putpixel(136,64,(88,88,88));
+putpixel(136,65,(32,32,32));
+putpixel(136,69,(8,8,8));
+putpixel(136,109,(32,32,32));
+putpixel(136,110,(104,104,104));
+putpixel(136,111,(191,191,191));
+for (x=136;x<137;x++){
+for(int y=209;y<218;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(137,51,(32,32,32));
+putpixel(137,60,(8,8,8));
+putpixel(137,115,(32,32,32));
+putpixel(137,116,(120,120,120));
+putpixel(137,117,(247,247,247));
+for (x=137;x<138;x++){
+for(int y=208;y<218;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(138,71,(56,56,56));
+putpixel(138,72,(104,104,104));
+putpixel(138,73,(151,151,151));
+putpixel(138,74,(199,199,199));
+putpixel(138,104,(207,207,207));
+putpixel(138,105,(112,112,112));
+putpixel(138,106,(32,32,32));
+putpixel(138,117,(40,40,40));
+putpixel(138,118,(199,199,199));
+for (x=138;x<139;x++){
+for(int y=208;y<217;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(139,62,(56,56,56));
+putpixel(139,63,(104,104,104));
+putpixel(139,64,(151,151,151));
+putpixel(139,65,(199,199,199));
+putpixel(139,109,(207,207,207));
+putpixel(139,110,(112,112,112));
+putpixel(139,111,(32,32,32));
+putpixel(139,119,(112,112,112));
+putpixel(139,120,(231,231,231));
+for (x=139;x<140;x++){
+for(int y=208;y<217;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(140,116,(16,16,16));
+putpixel(140,128,(32,32,32));
+putpixel(140,129,(151,151,151));
+for (x=140;x<141;x++){
+for(int y=208;y<217;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(141,117,(72,72,72));
+putpixel(141,133,(32,32,32));
+putpixel(141,134,(159,159,159));
+for (x=141;x<142;x++){
+for(int y=208;y<217;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(142,119,(64,64,64));
+putpixel(142,134,(8,8,8));
+putpixel(142,135,(159,159,159));
+for (x=142;x<143;x++){
+for(int y=208;y<218;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(143,128,(96,96,96));
+putpixel(143,135,(8,8,8));
+for (x=143;x<144;x++){
+for(int y=207;y<216;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(144,135,(104,104,104));
+for (x=144;x<167;x++){
+for(int y=207;y<437;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(167,73,(32,32,255));
+putpixel(167,74,(0,0,255));
+putpixel(167,75,(0,0,255));
+putpixel(167,76,(0,0,255));
+putpixel(167,77,(0,0,255));
+putpixel(167,78,(0,0,255));
+putpixel(167,79,(0,0,255));
+putpixel(167,80,(0,0,255));
+putpixel(167,81,(0,0,255));
+putpixel(167,82,(0,0,255));
+putpixel(167,83,(0,0,255));
+putpixel(167,84,(0,0,255));
+putpixel(167,85,(0,0,255));
+putpixel(167,86,(0,0,255));
+putpixel(167,87,(0,0,255));
+putpixel(167,88,(0,0,255));
+putpixel(167,89,(0,0,255));
+putpixel(167,90,(0,0,255));
+putpixel(167,91,(0,0,255));
+putpixel(167,92,(0,0,255));
+putpixel(167,93,(0,0,255));
+putpixel(167,94,(0,0,255));
+putpixel(167,95,(0,0,255));
+putpixel(167,96,(0,0,255));
+putpixel(167,97,(0,0,255));
+putpixel(167,98,(0,0,255));
+putpixel(167,99,(0,0,255));
+putpixel(167,100,(0,0,255));
+putpixel(167,101,(0,0,255));
+putpixel(167,102,(0,0,255));
+putpixel(167,103,(0,0,255));
+putpixel(167,104,(0,0,255));
+putpixel(167,105,(0,0,255));
+putpixel(167,106,(0,0,255));
+putpixel(167,107,(0,0,255));
+putpixel(167,108,(0,0,255));
+putpixel(167,109,(0,0,255));
+putpixel(167,110,(0,0,255));
+putpixel(167,111,(0,0,255));
+putpixel(167,112,(0,0,255));
+putpixel(167,113,(0,0,255));
+putpixel(167,114,(0,0,255));
+putpixel(167,115,(0,0,255));
+putpixel(167,116,(32,32,255));
+putpixel(167,117,(159,159,255));
+for (x=167;x<168;x++){
+for(int y=204;y<213;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(168,73,(0,0,255));
+putpixel(168,74,(0,0,255));
+putpixel(168,75,(0,0,255));
+putpixel(168,76,(0,0,255));
+putpixel(168,77,(0,0,255));
+putpixel(168,78,(0,0,255));
+putpixel(168,79,(0,0,255));
+putpixel(168,80,(0,0,255));
+putpixel(168,81,(0,0,255));
+putpixel(168,82,(0,0,255));
+putpixel(168,83,(0,0,255));
+putpixel(168,84,(0,0,255));
+putpixel(168,85,(0,0,255));
+putpixel(168,86,(0,0,255));
+putpixel(168,87,(0,0,255));
+putpixel(168,88,(0,0,255));
+putpixel(168,89,(0,0,255));
+putpixel(168,90,(0,0,255));
+putpixel(168,91,(0,0,255));
+putpixel(168,92,(0,0,255));
+putpixel(168,93,(0,0,255));
+putpixel(168,94,(0,0,255));
+putpixel(168,95,(0,0,255));
+putpixel(168,96,(0,0,255));
+putpixel(168,97,(0,0,255));
+putpixel(168,98,(0,0,255));
+putpixel(168,99,(0,0,255));
+putpixel(168,100,(0,0,255));
+putpixel(168,101,(0,0,255));
+putpixel(168,102,(0,0,255));
+putpixel(168,103,(0,0,255));
+putpixel(168,104,(0,0,255));
+putpixel(168,105,(0,0,255));
+putpixel(168,106,(0,0,255));
+putpixel(168,107,(0,0,255));
+putpixel(168,108,(0,0,255));
+putpixel(168,109,(0,0,255));
+putpixel(168,110,(0,0,255));
+putpixel(168,111,(0,0,255));
+putpixel(168,112,(0,0,255));
+putpixel(168,113,(0,0,255));
+putpixel(168,114,(0,0,255));
+putpixel(168,115,(0,0,255));
+putpixel(168,116,(0,0,255));
+putpixel(168,117,(8,8,255));
+for (x=168;x<169;x++){
+for(int y=204;y<213;y++) {putpixel(x,y,WHITE);
+}}
+putpixel(169,73,(0,0,255));
+putpixel(169,74,(0,0,255));
+putpixel(169,75,(0,0,255));
+putpixel(169,76,(0,0,255));
+putpixel(169,77,(0,0,255));
+putpixel(169,78,(0,0,255));
+putpixel(169,79,(0,0,255));
+putpixel(169,80,(0,0,255));
+putpixel(169,81,(0,0,255));
+putpixel(169,82,(0,0,255));
+putpixel(169,83,(0,0,255));
+putpixel(169,84,(0,0,255));
+putpixel(169,85,(0,0,255));
+putpixel(169,86,(0,0,255));
+putpixel(169,87,(0,0,255));
+putpixel(169,88,(0,0,255));
+putpixel(169,89,(0,0,255));
+putpixel(169,90,(0,0,255));
+putpixel(169,91,(0,0,255));
+putpixel(169,92,(0,0,255));
+putpixel(169,93,(0,0,255));
+putpixel(169,94,(0,0,255));
+putpixel(169,95,(0,0,255));
+putpixel(169,96,(0,0,255));
+putpixel(169,97,(0,0,255));
+putpixel(169,98,(0,0,255));
+putpixel(169,99,(0,0,255));
+putpixel(169,100,(0,0,255));
+putpixel(169,101,(0,0,255));
+putpixel(169,102,(0,0,255));
+putpixel(169,103,(0,0,255));
+putpixel(169,104,(0,0,255));
+putpixel(169,105,(0,0,255));
+putpixel(169,106,(0,0,255));
+putpixel(169,107,(0,0,255));
+putpixel(169,108,(0,0,255));
+putpixel(169,109,(0,0,255));
+putpixel(169,110,(0,0,255));
+putpixel(169,111,(0,0,255));
+putpixel(169,112,(0,0,255));
+putpixel(169,113,(0,0,255));
+putpixel(169,114,(0,0,255));
+putpixel(169,115,(0,0,255));
+putpixel(169,116,(0,0,255));
+putpixel(169,117,(104,104,255));getch();
+
+closegraph(); 
+}
--- img.py
+++ img.py
@@ -2,20 +2,47 @@ from cv2 import imread
 import numpy as np
 f=open("cfile.cpp","w+")
 img=imread('temp.png')
+cppcode='#include<iostream.h>\n \
+#include<conio.h>\n \
+#include <graphics.h>\n \
+void main()\n \
+{ int i,x,y;\n \
+int gd=DETECT,gm,color;\n \
+initgraph(&gd,&gm,"c://turboc3//bgi");\n'
 
-f.write('#include<iostream.h>\n#include<conio.h>\n#include <graphics.h>\nvoid main()\n{ int i;\nint gd=DETECT,gm,color;\n initgraph(&gd,&gm,"c://turboc3//bgi");\n')
 x=[]
 y=np.array([0,0,0])
+count=0
+
+x=0
+y=0
+ix=0
+jy=0
 for i in range(0,img.shape[0]):
+    ix+=1
     for j in range(0,img.shape[1]):
 
-        if np.array_equal(img[i][j], [0,0,0]) is True: 
+        if np.array_equal(img[i][j], [0,0,0]) is False: 
+            if np.array_equal(img[i][j],[255,255,255]) is True and count==0:
+                count=1
+                ix=i
+                jy=j
+                x=i
+                y=j
+            elif np.array_equal(img[i][j],[255,255,255]) is True:
+                count=1
+                jy+=1
+            elif count==1:
+                count=0
+                cppcode+='\nfor (x='+str(x)+';x<'+str(ix)+';x++){\nfor(int y='+str(y)+';y<'+str(jy)+';y++) {putpixel(x,y,WHITE);\n}}'
             #print('Cordinates:({},{}) rgbcode:{}'.format(i,j,img[i][j]))
-            yup='\nputpixel({},{},WHITE);'.format(i,j)
- #           print(yup)
-            f.write(yup)
+            else:
+                cppcode+='\nputpixel({},{},({},{},{}));'.format(i,j,img[i][j][0],img[i][j][1],img[i][j][2])
+
             
-f.write("getch();\n\nclosegraph(); \n}")
+cppcode+="getch();\n\nclosegraph(); \n}"    
+print(cppcode)
+f.write(cppcode)
 f.close()
 print("ok")
 
